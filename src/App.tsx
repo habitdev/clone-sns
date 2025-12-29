3acb8e53afa30c18e024d02c80c6af07765c009d
habitdev <goodstory16@gmail.com>
2025-12-29 14:17:26 +0900

    feat: Integrate shadcn/ui components, Tailwind CSS, and Prettier for enhanced styling and UI development.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
-import './App.css';
+import "./App.css";
+import { Button } from "./components/ui/button";
+import { cn } from "./lib/utils";
 
 function App() {
-
-  return <></>;
+  const isActive = true;
+  return (
+    <div>
+      <Button>버튼!</Button>
+      <div className="text-primary">Primary</div>
+      <div className={cn(isActive ? "text-green-500" : "text-red-500")}>
+        isActive
+      </div>
+      <div className="text-muted">Muted</div>
+      <div className="text-destructive">Destructive</div>
+    </div>
+  );
 }
 
 export default App;
