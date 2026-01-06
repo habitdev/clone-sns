9ee1b2605dc6192dcf068ae0c5cfb8e34c78907c
habitdev <goodstory16@gmail.com>
2026-01-06 14:01:17 +0900

    feat: combine middelware사용

--- src/store/count.ts
+++ src/store/count.ts
@@ -1,5 +1,7 @@
 import { create } from "zustand";
+import { combine } from "zustand/middleware";
 
+/*
 interface Store {
   count: number;
   // actions로 감싸면 하나로 불러와서 사용할 수 있다
@@ -9,7 +11,28 @@ interface Store {
     decreaseOne: () => void;
   };
 }
+*/
 
+// combine을 사용하는 이유:
+// 첫번째로 넘기는 state의 타입이 자동으로 추론이 되기 때문이다
+// 보통 set((store)부분을 오해하지 않도록 set((state)로 바꿔서 입력한다
+export const useCountStore = create(
+  combine({ count: 0 }, (set, get) => ({
+    actions: {
+      increaseOne: () => {
+        // const count = get().count;
+        // set({ count: count + 1 });
+
+        set((state) => ({ count: state.count + 1 }));
+      },
+      decreaseOne: () => {
+        set((state) => ({ count: state.count - 1 }));
+      },
+    },
+  })),
+);
+
+/*
 export const useCountStore = create<Store>((set, get) => ({
   count: 0,
   actions: {
@@ -24,6 +47,7 @@ export const useCountStore = create<Store>((set, get) => ({
     },
   },
 }));
+*/
 
 // 유지보수가 쉽도록
 export const useCount = () => {
