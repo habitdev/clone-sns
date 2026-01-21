44b6113c9139d6e41ba867576f7a2b9ad9071d43
habitdev <goodstory16@gmail.com>
2026-01-21 16:33:10 +0900

    feat: mutation의 onSucess를 이용한 데이터 업데이트

--- src/hooks/mutations/use-create-todo-mutation.ts
+++ src/hooks/mutations/use-create-todo-mutation.ts
@@ -9,12 +9,13 @@ export function useCreateTodoMutation() {
     mutationFn: createTodo,
     onMutate: () => {},
     onSettled: () => {},
-    onSuccess: () => {
-      queryClient.invalidateQueries({
-        queryKey: QUERY_KEYS.todo.list,
-        // todo 리스트의 캐시 데이터를 무효화해서 데이터를 다시 불러오게 한다(새로고침 효과)
-        // 데이터가 너무 많거나 자주 refetching을 할 경우 서버에 부하가 일어날 수 있다
+    onSuccess: (newTodo) => {
+      queryClient.setQueryData<Todo[]>(QUERY_KEYS.todo.list, (prevTodos) => {
+        if (!prevTodos) return [newTodo];
+        return [...prevTodos, newTodo];
       });
+      // 데이터 Refetch없이 변경된 데이터를 화면에 반영하는 방법
+      // onSuccess는 매개변수로 반환된 데이터를 받을 수 있다.
     },
     onError: () => {},
   });
